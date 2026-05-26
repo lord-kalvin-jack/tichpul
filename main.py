fa210e299d2112d0451ee20e3e3347e792c7a212
= <=>
2026-05-26 15:04:43 +0300

    finished exercise

--- main.py
+++ main.py
@@ -1,34 +1,22 @@
-from functools import reduce
-import operator
 #Binyamin Godfrey 218804581
 #Elad Zwecher 218779668
 #------------------------------q1------------------------------
-def q1():
-    file_name = input("Enter project file name: ")
-    #input validation is made in our get_proj_info
-    print("Project Times Delta:")
-    print(proj_times(file_name))
-    
-    cost_per_time = float(input("Enter cost per time unit: "))
-    print("\nProject Time and Cost (Estimated, Actual, Delta):")
-    print(proj_time_cost(file_name, cost_per_time))
-
 def proj_times(file_name):
     lst = get_proj_info(file_name)
 
     def processProj(nodes):
         if not nodes:
             return []
-        
-        return [[[node[0][0], node[0][2] - node[0][1]], processProj(node[1])] for node in nodes]
+        return [[[node[0][0], int(node[0][2]) - int(node[0][1])], processProj(node[1])] for node in nodes]
         
     return processProj(lst)
 
+
 def proj_time_cost(file_name, cost_per_time_unit):
     nodes = get_proj_info(file_name)
-    
-    estimatedTime = sum([node[0][1] for node in nodes])
-    actualTime = sum([node[0][2] for node in nodes])
+
+    estimatedTime = sum([int(node[0][1]) for node in nodes])
+    actualTime = sum([int(node[0][2]) for node in nodes])
     timeDelta = actualTime - estimatedTime
 
     return (
@@ -40,13 +28,28 @@ def proj_time_cost(file_name, cost_per_time_unit):
 def get_proj_info(file_name):
     try:
         with open(file_name, "r") as flobj:
-            lines = flobj.readlines()
-
+            return eval(flobj.read()) 
     except:
-        print("An error occurred. File name may not be valid.")
-        return []
+        return None
+    
+def q1():
+    file_name = input("Enter project file name: ")
+    
+    if get_proj_info(file_name) == None:
+        print("ERROR: File not found or invalid format.")
+        return 
+        
+    print("Project Times Delta:")
+    print(proj_times(file_name))
+    
+    cost_per_time = eval(input("Enter cost per time unit: "))
+    if not isinstance(cost_per_time, (int, float)) or cost_per_time <= 0:
+        print("ERROR: Invalid cost.")
+        return
+        
+    print("\nProject Time and Cost (Estimated, Actual, Delta):")
+    print(proj_time_cost(file_name, cost_per_time))
 
-    return [line[:-1].split(",") for line in lines]
 #------------------------------q2------------------------------
 def reverseList(n):
     if isinstance(n, (int , float)):
@@ -61,10 +64,11 @@ def reverseList(n):
         return [reverseList(item) for item in n][::-1]
     else:
         return [n]
+
 def q2():
     n = eval(input("Enter a list: "))
     
-    if (not isinstance(list)):
+    if not isinstance(n, list):
         print("ERROR: Input list is incorrect! ")
         return
     
@@ -73,24 +77,25 @@ def q2():
 #------------------------------q3------------------------------
 def isPalindrome(n):
      return n == reverseList(n)
+
 def q3():
     n = eval(input("Enter a list: "))
 
-    if (not isinstance(list)):
+    if not isinstance(n, list):
         print("ERROR: Input list is incorrect! ")
         return
     
     if isPalindrome(n):
         print("It is a palindrome")
+    else: 
+        print("It is not a palindrome")
 
-    else: print("It is not a palindrome")
 #------------------------------q4------------------------------
-def primes(n):
-    compositeNumbers = {j for i in range(2, int(n**0.5) + 1) for j in range(i * 2, n + 1, i)}
-
-    return [i for i in range(2,n + 1) if i not in compositeNumbers]
-
 def twinp(n):
+    def primes(n):
+        compositeNumbers = {j for i in range(2, int(n**0.5) + 1) for j in range(i * 2, n + 1, i)}
+        return [i for i in range(2,n + 1) if i not in compositeNumbers]
+
     primeList = primes(n)
     return {prime : prime + 2 for prime in primeList if prime + 2 in primeList}
 
@@ -102,16 +107,23 @@ def q4():
         return
     prime_dict = twinp(n)
 
-    [print(f"{key} {value}") for key, value in twinp]
+    if prime_dict:
+        print('\n'.join([f"{key} {value}" for key, value in prime_dict.items()]))
       
 #------------------------------q5------------------------------
 def add3dicts(d1,d2,d3):
     dicts = (d1, d2, d3)
-    all_keys = set().union(*(d.keys() for d in dicts))
+    all_keys = set().union(*[(d.keys() for d in dicts)])
+    
+    sharedKeys = lambda s1, s2, s3: (s1 & s2) | (s2 & s3) | (s1 & s3)
+    foreignKeys = lambda s1, s2, s3: (s1 | s2 | s3) - sharedKeys(s1, s2, s3)
+    shared = sharedKeys(set(d1.keys()), set(d2.keys()), set(d3.keys()))
+    
     def merge(key):
-        values = tuple(d[key] for d in dicts if key in d)
+        values = tuple([d[key] for d in dicts if key in d])        
         unique_values = tuple(dict.fromkeys(values))
-        return unique_values[0] if len(unique_values) == 1 else unique_values
+        return unique_values if key in shared else unique_values[0]
+        
     return {k: merge(k) for k in all_keys}
 
 def q5():
@@ -158,7 +170,7 @@ def main():
             print("Exiting...")
             break
         elif 1 <= c <= len(lstrs):
-            lfuncs[c - 1]() # Execute the chosen function
+            lfuncs[c - 1]() 
         else:
             print("ERROR: Choice out of range.")
 
